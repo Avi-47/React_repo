7950590a362a08ae2192e2a443c15dfff096148c
Avi-47 <avimanyug.ee.ug@jadavpuruniversity.in>
2026-01-05 00:21:46 +0530

    Add React projects: 01basicreact and 01vitereact

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1> Chai aur React | Hey Avimanyu here today I started learning React.js and learnt to install it in my system also vite.js in the system. So lets start learning React.js. Also learnt to create a basic react app using vite.js and react.js.</h1>
+  );
+}
+
+export default App;
