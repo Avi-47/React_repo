ddaa846749b12f631410b74e2649d0027d61d936
Avi-47 <avimanyug.ee.ug@jadavpuruniversity.in>
2026-01-31 18:18:27 +0530

    context api discussion

---
+++ src/main.jsx
@@ -0,0 +1,5 @@
+// Import Tailwind CSS
+import './index.css'
+
+// When you set up your React app, import this CSS file in your main entry point:
+// Example: import './index.css' in your main.jsx or index.js file
